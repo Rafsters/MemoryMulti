e6460a211ac7bcb0f02af3bd92b29455ab8443a4
Rafal Kwiecien <Rafsters@interia.pl>
2017-06-10 19:55:02 +0200

    Added some JLabels and JPanel, Counting points implemented

--- src/test/Board.java
+++ src/test/Board.java
@@ -1,4 +1,4 @@
-package test;
+
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -17,12 +17,42 @@ public class Board extends JFrame {
     private Card c1;
     private Card c2;
     private Timer t;
-
+    private JPanel boardPanel, pointsPanel;
+    private JLabel player1, player2, player1Points, player2Points, round, roundPlayer;
+    private int p1Points, p2Points;
 
     public Board() {
         int pairs = 10;
+        p1Points = 0;
+        p2Points = 0;
         List<Card> cardsList = new ArrayList<Card>();
         List<Integer> cardVals = new ArrayList<Integer>();
+        setLayout(new BorderLayout());
+        boardPanel = new JPanel();
+        pointsPanel = new JPanel();
+        pointsPanel.setLayout(new GridLayout(1, 6));
+        pointsPanel.setBackground(new Color(200, 233, 255));
+        player1 = new JLabel("Gracz 1");
+        player1Points = new JLabel("<html><font color=#000066>" + p1Points + "</font></html>");
+        player2 = new JLabel("Gracz 2");
+        player2Points = new JLabel("<html><font color=#000066>" + p2Points + "</font></html>");
+        round = new JLabel("Tura: ");
+        roundPlayer = new JLabel("<html><font color=#660000>" + isPlaying() + "</font></html>");
+        player1.setFont(new Font("Serif", Font.PLAIN, 18));
+        player2.setFont(new Font("Serif", Font.PLAIN, 18));
+        player1Points.setFont(new Font("Serif", Font.PLAIN, 22));
+        player2Points.setFont(new Font("Serif", Font.PLAIN, 22));
+        round.setFont(new Font("Serif", Font.PLAIN, 18));
+        roundPlayer.setFont(new Font("Serif", Font.PLAIN, 22));
+        pointsPanel.add(player1);
+        pointsPanel.add(player1Points);
+        pointsPanel.add(player2);
+        pointsPanel.add(player2Points);
+        pointsPanel.add(round);
+        pointsPanel.add(roundPlayer);
+        add(boardPanel, BorderLayout.CENTER);
+        add(pointsPanel, BorderLayout.SOUTH);
+        boardPanel.setLayout(new GridLayout(4, 5));
 
         for (int i = 0; i < pairs; i++) {
             cardVals.add(i);
@@ -52,11 +82,12 @@ public class Board extends JFrame {
         t.setRepeats(false);
 
         //set up the board itself
-        Container pane = getContentPane();
-        pane.setLayout(new GridLayout(4, 5));
+        
+        //Container pane = getContentPane();
+        //pane.setLayout(new GridLayout(4, 5));
         for (Card c : cards) {
             c.setIcon(new ImageIcon(setBlank()));
-            pane.add(c);
+           boardPanel.add(c);
         }
         setTitle("MemoryMulti");
     }
@@ -70,8 +101,11 @@ public class Board extends JFrame {
         }
         return card;
     }
-
+    
+    boolean player1IsMakingTurn = true;
+    boolean player2IsMakingTurn = false;
     public void doTurn() {
+    	System.out.println("1: " + p1Points + ", 2: " + p2Points);
         if (c1 == null && c2 == null) {
             c1 = selectedCard;
             c1.setCardImage(c1.getId());
@@ -83,16 +117,67 @@ public class Board extends JFrame {
             t.start();
 
         }
+        
+    }
+    
+    public boolean isPlayer1MakingTurn(){
+    	if(player1IsMakingTurn == true && player2IsMakingTurn == false ){
+    		return true;
+    	} else if(player1IsMakingTurn == false && player2IsMakingTurn == false ){
+    		return true;
+    	}else{
+    		return false;
+    	}
+    		
+    }
+    
+    public boolean isPlayer2MakingTurn(){
+    	if(player1IsMakingTurn == false && player2IsMakingTurn == true ){
+    		return true;
+    	} else{
+    		return false;
+    	}
+    		
+    }
+    
+    public String isPlaying(){
+    	if(isPlayer1MakingTurn()){
+    		return "Gracz 1";
+    	} else if(isPlayer2MakingTurn()) {
+    		return "Gracz 2";
+    	}
+		return null;
     }
 
     public void checkCards() {
+    	
+    	if(player1IsMakingTurn == true && player2IsMakingTurn == false ) {
+        	player1IsMakingTurn = false;
+        	player2IsMakingTurn = true;
+        } else if(player1IsMakingTurn == false && player2IsMakingTurn == true ) {
+        	player1IsMakingTurn = true;
+        	player2IsMakingTurn = false;
+        }
         if (c1.getId() == c2.getId()) {//match condition
             c1.setEnabled(false); //disables the button
             c2.setEnabled(false);
             c1.setMatched(true); //flags the button as having been matched
             c2.setMatched(true);
+            if(isPlayer1MakingTurn()){
+            	p1Points++;
+            	player1Points.setText("<html><font color=#006600>" + p1Points + "</font></html>");
+            }else if(isPlayer2MakingTurn()){
+            	p2Points++;
+            	player2Points.setText("<html><font color=#000066>" + p2Points + "</font></html>");
+            }
             if (this.isGameWon()) {
-                JOptionPane.showMessageDialog(this, "You win!");
+            	if(p1Points > p2Points){
+            		JOptionPane.showMessageDialog(this, "Gracz 1 wygrywa!");
+            	} else if(p1Points < p2Points){
+            		JOptionPane.showMessageDialog(this, "Gracz 2 wygrywa!");
+            	} else{
+            		JOptionPane.showMessageDialog(this, "Remis!");
+            	}
                 System.exit(0);
             }
         } else {
@@ -101,6 +186,7 @@ public class Board extends JFrame {
         }
         c1 = null; //reset c1 and c2
         c2 = null;
+        roundPlayer.setText("<html><font color=#660000>" + isPlaying() + "</font></html>");
     }
 
     public boolean isGameWon() {
